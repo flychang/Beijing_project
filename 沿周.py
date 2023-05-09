ab019e7811fe810d9fd6b32559208545c2c2351a
flychang <2311565264@qq.com>
2023-05-09 09:55:07 +0800

    偏移量+逻辑判断

--- 沿周.py
+++ 沿周.py
@@ -53,3 +53,39 @@ def get_mouse_pos(event, x, y, flags, param):
         # draw.text((100, 350), "你好", font=font, fill=(255, 255, 255))
         # img = np.array(img_pil)
         # cv2.imshow("add_text", img)
+     #大图坐标转小图
+    if event == cv2.EVENT_MOUSEMOVE:
+
+        mx, my = x, y
+
+        img_draw = img_copy.copy()
+        print("鼠标坐标：", (x, y))
+        angle_rad = 0
+        # 把左右圆心标注出来
+        img_draw[lcy - 1:lcy + 1, lcx - 1:lcx + 1] = [255, 255, 255]
+        img_draw[rcy - 1:rcy + 1, rcx - 1:rcx + 1] = [255, 255, 255]
+        if lcx > mx >= 0:
+            x1, y1, x2, y2 = get_cercle(lcx, lcy)
+            # 大图坐标转小图坐标，调用亿力的函数
+            flag, width = MouseMinWidth.MouseMinWidthInRing(x2 - 22, y2 - 96, x1 - 22, y1 - 96, xiaoleft)
+        # 计算中间上部条形
+        if lcx <= mx < rcx and 0 < my <= rcy:
+            x1 = x2 = mx
+            y1 = lcy - radius1
+            y2 = lcy - radius0
+            # 大图坐标转小图坐标，调用亿力的函数
+            xx = x1 - lcx
+            flag, width = MouseMinWidth.MouseMinWidthInLine(xx, xiaoup)
+        # 计算中间下部条形
+        if lcx <= mx < rcx and my > rcy:
+            x1 = x2 = mx
+            y1 = lcy + radius1
+            y2 = lcy + radius0
+            # 大图坐标转小图坐标，调用亿力的函数
+            xx = x1 - lcx
+            flag, width = MouseMinWidth.MouseMinWidthInLine(xx, xiaodown)
+        # 计算右侧半圆弧度值
+        if mx >= rcx:
+            x1, y1, x2, y2 = get_cercle(rcx, rcy)
+            # 大图坐标转小图坐标，调用亿力的函数
+            flag, width = MouseMinWidth.MouseMinWidthInRing(x2 - 1142, y2 - 96, x1 - 1142, y1 - 96, xiaoright)
